0351940b05473ac46d87f35ddcbbd985c69ec498
Louis Shi <louis.shi@uconn.edu>
2018-01-20 16:38:00 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -11,8 +11,9 @@ function whileLoop(n) {
   return 'done'
 }
 function doWhileLoop(array) {
-    return array.pop()
-  }
+  function maybeTrue() {
+  return array.pop()
+}
 do {
   console.log('doo-bee-doo-bee-doo')
 } while (maybeTrue() && array.length > 0)
