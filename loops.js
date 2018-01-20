a96891c790e7ce6bbe4dd11e1ed7d79c410cf5dd
Louis Shi <louis.shi@uconn.edu>
2018-01-20 16:05:57 +0000

    automatically backed up by learn

---
+++ loops.js
@@ -0,0 +1,21 @@
+function forLoop(array) {
+  for (var i = 0; i < 25; i++) {
+    array[i] = "I am ${i} strange loops" + array[i]
+  }
+  return array
+}
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(--n)
+  }
+  return 'done'
+}
+function doWhileLoop(array) {
+  function maybeTrue() {
+  return array
+}
+do {
+  console.log('')
+} while (maybeTrue())
+return array
+}
